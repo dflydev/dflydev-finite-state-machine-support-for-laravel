f699d7e2cee0786cb835c91e3ced94ecf9d1f991
Zaher Ghaibeh <zaher@zaher.dev>
2023-05-03 22:01:58 +0300

    Adding php-cs-fixer GitHub workflow

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,80 +2,24 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Config;
-use PhpCsFixer\Finder;
+use Ergebnis\License;
 
-$rules = [
-    '@PER' => true,
-    '@Symfony' => true,
+$license = License\Type\MIT::text(__DIR__ . '/LICENSE',
+    License\Range::since(
+        License\Year::fromString(date('Y')),
+        new \DateTimeZone('UTC')
+    ),
+    License\Holder::fromString('Dragonfly Development Inc'),
+    License\Url::fromString('https://github.com/dflydev/dflydev-finite-state-machine-support-for-laravel')
+);
 
-    'declare_strict_types' => true,
+$license->save();
 
-    // Override @Symfony
+use Ergebnis\PhpCsFixer\Config;
 
-    'phpdoc_align' => [
-        'align' => 'left',
-        'tags' => [
-            'method',
-            'param',
-            'property',
-            'property-read',
-            'property-write',
-            'return',
-            'throws',
-            'type',
-            'var',
-        ],
-    ],
+$config = Config\Factory::fromRuleSet(new Dflydev\PhpCsFixer\Config\RuleSet\Dflydev($license->header()));
 
-    'phpdoc_separation' => [
-        'groups' => [
-            // Defaults
-            ['deprecated', 'link', 'see', 'since'],
-            ['author', 'copyright', 'license'],
-            ['category', 'package', 'subpackage'],
-            ['property', 'property-read', 'property-write'],
+$config->getFinder()->in(__DIR__);
+$config->setCacheFile(__DIR__ . '/.php-cs-fixer.cache');
 
-            // Overrides
-            ['template', 'template-covariant', 'template-uses'],
-
-            ['uses'],
-
-            ['phpstan-*'], // This is not available... yet.
-
-            ['phpstan-param', 'phpstan-return'],
-        ],
-    ],
-
-    'global_namespace_import' => [
-        'import_classes' => null,
-        'import_constants' => true,
-        'import_functions' => true,
-    ],
-    'no_empty_comment' => false,
-    'php_unit_method_casing' => ['case' => 'snake_case'],
-    'single_line_throw' => false,
-    'yoda_style' => false,
-
-    'phpdoc_to_comment' => [
-        'ignored_tags' => [
-            'array',
-            'var',
-            'phpstan-var',
-        ],
-    ],
-];
-
-$finder = Finder::create()
-    ->in([
-        __DIR__.'/src',
-    ])
-    ->name('*.php')
-    ->ignoreDotFiles(true)
-    ->ignoreVCS(true);
-
-return (new Config())
-    ->setFinder($finder)
-    ->setRules($rules)
-    ->setRiskyAllowed(true)
-    ->setUsingCache(true);
+return $config;
