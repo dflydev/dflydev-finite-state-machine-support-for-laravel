be37501ef3b7709346742f30a6c01cfe5b7d4e36
Zaher Ghaibeh <zaher@zaher.dev>
2023-05-04 21:25:44 +0300

    remove the license header

--- src/UnableToChangeState.php
+++ src/UnableToChangeState.php
@@ -2,15 +2,6 @@
 
 declare(strict_types=1);
 
-/**
- * Copyright (c) 2023 Dragonfly Development Inc
- *
- * For the full copyright and license information, please view
- * the LICENSE file that was distributed with this source code.
- *
- * @see https://github.com/dflydev/dflydev-finite-state-machine-support-for-laravel
- */
-
 namespace Dflydev\FiniteStateMachine\SupportForLaravel;
 
 use RuntimeException;
